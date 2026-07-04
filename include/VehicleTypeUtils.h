ce7b6c9edc9d34ee275e92fb0eb6cd515e9967f0
emeliefogelstrom <isladesign82@gmail.com>
2026-07-04 22:33:46 +0200

    Add VehicleTypeUtils namespace for type code conversion

---
+++ include/VehicleTypeUtils.h
@@ -0,0 +1,47 @@
+#ifndef VEHICLETYPEUTILS_H
+#define VEHICLETYPEUTILS_H
+#include "CarriageType.h"
+#include "LocomotiveType.h"
+#include <stdexcept>
+#include <string>
+
+namespace VehicleTypeUtils
+{
+    bool isLocomotive(int type)
+    {
+        return type == 4 || type == 5;
+    }
+
+    CarriageType convertCarriageType(int typeNumber)
+    {
+        switch (typeNumber)
+        {
+        case 0:
+            return CarriageType::Seat;
+        case 1:
+            return CarriageType::Sleeping;
+        case 2:
+            return CarriageType::OpenFreight;
+        case 3:
+            return CarriageType::CoveredFreight;
+        default:
+            throw std::invalid_argument("Unknown carriage type: " + std::to_string(typeNumber));
+        }
+    }
+
+    LocomotiveType convertLocomotiveType(int typeNumber)
+    {
+
+        switch (typeNumber)
+        {
+        case 4:
+            return LocomotiveType::Electric;
+        case 5:
+            return LocomotiveType::Diesel;
+        default:
+            throw std::invalid_argument("Unknown locomotive type: " + std::to_string(typeNumber));
+        }
+    }
+};
+
+#endif
