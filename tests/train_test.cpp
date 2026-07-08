92e5fb7a8d14b98dd078c649ee80220648f992f4
emeliefogelstrom <isladesign82@gmail.com>
2026-07-08 22:24:13 +0200

    Add unit test for Train::finish
    Verifies vehicles are moved from escrow to arrival station pool
    and status becomes FINISHED after finish() is called.

--- tests/train_test.cpp
+++ tests/train_test.cpp
@@ -59,3 +59,25 @@ TEST(TrainTest, AssemblesTrainWithOnlyOneElectricLocomotiveAvailableStatusIsInco
     EXPECT_EQ(departure.getLocomotives().size(), 1);
     EXPECT_EQ(box.getStorageSize(), 0);
 }
+
+TEST(TrainTest, DisassembleTrainAndMoveVehiclesToArrivalStation)
+{
+    Station departure("Grand Central");
+    departure.addLocomotive(std::make_unique<ElectricLocomotive>(1, 125, 1542));
+    departure.addCarriage(std::make_unique<SeatCarriage>(2, 125, 0));
+    departure.addCarriage(std::make_unique<CoveredFreightCarriage>(3, 50));
+    departure.addCarriage(std::make_unique<CoveredFreightCarriage>(4, 50));
+    departure.addLocomotive(std::make_unique<ElectricLocomotive>(5, 123, 1462));
+
+    VehicleEscrow box;
+    Train train(123, "Grand Central", "St. Pancras", 765, 965, 145, {4, 0, 3, 3, 4});
+    train.tryAssemble(departure, box);
+
+    Station arrival("St. Pancras");
+    train.finish(arrival, box);
+
+    EXPECT_EQ(arrival.getCarriages().size(), 3);
+    EXPECT_EQ(arrival.getLocomotives().size(), 2);
+    EXPECT_EQ(box.getStorageSize(), 0);
+    EXPECT_EQ(train.getStatus(), TrainStatus::FINISHED);
+}
