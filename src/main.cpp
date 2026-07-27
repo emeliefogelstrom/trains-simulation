13b60c750abd18884e93da8238018fefe66b6785
emeliefogelstrom <isladesign82@gmail.com>
2026-07-28 00:11:05 +0200

    Update CMakeLists and main.cpp for UserInterface

--- src/main.cpp
+++ src/main.cpp
@@ -6,49 +6,28 @@
 #include "OpenFreightCarriage.h"
 #include "CoveredFreightCarriage.h"
 #include "Station.h"
+#include "UserInterface.h"
+#include <FileParser.h>
 
 int main()
 {
-    ElectricLocomotive elLoco{70, 224, 4955};
-    std::cout << "ElectricLocomotive id=" << elLoco.getId()
-              << " maxSpeed=" << elLoco.getMaxSpeed()
-              << " consumption=" << elLoco.getConsumption() << "\n";
+    std::cout << "========================================\n"
+              << "   Ironbend Train and Brain Railway\n"
+              << "         Train Simulation v1.0\n"
+              << "========================================\n\n"
+              << "Loading simulation data...\n";
 
-    DieselLocomotive dieselLoco{71, 180, 350};
-    std::cout << "DieselLocomotive id=" << dieselLoco.getId()
-              << " maxSpeed=" << dieselLoco.getMaxSpeed()
-              << " consumption=" << dieselLoco.getConsumption() << "\n";
+    auto tracks = FileParser::parseTrainMap("../files/TrainMap.txt");
+    auto stations = FileParser::parseTrainStations("../files/TrainStations.txt");
+    auto trains = FileParser::parseTrains("../files/Trains.txt", tracks);
 
-    SeatCarriage seat{1, 100, true};
-    std::cout << "SeatCarriage id=" << seat.getId()
-              << " seats=" << seat.getSeats() << "\n";
+    std::cout << "Loaded " << trains.size() << " trains, "
+              << stations.size() << " stations, "
+              << tracks.size() << " tracks.\n\n";
 
-    SleepingCarriage sleeping{2, 30};
-    std::cout << "SleepingCarriage id=" << sleeping.getId()
-              << " beds=" << sleeping.getBeds() << "\n";
+    Simulation sim(std::move(trains), std::move(stations), tracks);
+    UserInterface ui(sim);
+    ui.run();
 
-    OpenFreightCarriage openFreight{3, 50, 20};
-    std::cout << "OpenFreightCarriage id=" << openFreight.getId() << "\n";
-
-    CoveredFreightCarriage coveredFreight{4, 97};
-    std::cout << "CoveredFreightCarriage id=" << coveredFreight.getId() << "\n";
-
-    Station station("Grand Central Station");
-    std::cout << "Station name: " << station.getStationName() << "\n";
-    auto locomotive = std::make_unique<ElectricLocomotive>(5, 167, 4538);
-    station.addLocomotive(std::move(locomotive));
-    Locomotive *foundLocomotive = station.getLocomotiveById(5);
-    if (foundLocomotive)
-    {
-        std::cout << "Found locomotive id= " << foundLocomotive->getId()
-                  << " maxSpeed= " << foundLocomotive->getMaxSpeed() << "\n";
-    }
-    auto carriage = std::make_unique<SeatCarriage>(3, 125, 1);
-    station.addCarriage(std::move(carriage));
-    Carriage *foundCarriage = station.getCarriageById(3);
-    if (foundCarriage)
-    {
-        std::cout << "Found carriage id= " << foundCarriage->getId() << "\n";
-    }
     return 0;
 }
