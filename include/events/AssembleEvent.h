07317de97340c6eeea10c4c7db4f697fbf6a5a17
emeliefogelstrom <isladesign82@gmail.com>
2026-07-31 15:28:45 +0200

    Add endTime parameter to AssembleEvent to stop scheduling after end time

--- include/events/AssembleEvent.h
+++ include/events/AssembleEvent.h
@@ -10,23 +10,22 @@ class AssembleEvent : public Event
 private:
     Station &station_;
     VehicleEscrow &vehicleEscrow_;
+    int endTime_;
 
 public:
-    AssembleEvent(Train &train, int time, Station &station, VehicleEscrow &vehicleEscrow) : Event(train, time), station_(station), vehicleEscrow_(vehicleEscrow) {}
+    AssembleEvent(Train &train, int time, Station &station, VehicleEscrow &vehicleEscrow, int endTime)
+        : Event(train, time), station_(station), vehicleEscrow_(vehicleEscrow), endTime_(endTime) {}
 
     std::unique_ptr<Event> processEvent() override
     {
-
-        if (time_ >= 1440)
-        {
+        if (time_ >= endTime_)
             return nullptr;
-        }
 
         if (train_.tryAssemble(station_, vehicleEscrow_))
         {
             return std::make_unique<ReadyEvent>(
                 train_,
-                train_.getScheduledDepartureTime() - 10);
+                train_.getScheduledDepartureTime() + train_.getDelay() - 10);
         }
 
         train_.addDelay(10);
@@ -35,7 +34,8 @@ public:
             train_,
             time_ + 10,
             station_,
-            vehicleEscrow_);
+            vehicleEscrow_,
+            endTime_);
     }
 };
 
